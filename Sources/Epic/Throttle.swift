49b35c690d2662fea553d338fa6c7f9f7f410732
Miguel Hernández Jaso <maquert@me.com>
2021-05-29 12:42:54 +0200

    Clean up the throttle to allow some future DI

--- Sources/Epic/Throttle.swift
+++ Sources/Epic/Throttle.swift
@@ -4,8 +4,6 @@ import Foundation
 /// Within every interval, only the last throttled task will be executed.
 /// Tasks are always dispatched on the main queue.
 public final class Throttle: ThrottleProtocol {
-    fileprivate typealias Me = Throttle
-
     private let queue: DispatchQueue = DispatchQueue.global(qos: .background)
     private var task: DispatchWorkItem = DispatchWorkItem(block: {})
     private var previousExecution: Date = Date.distantPast
@@ -17,19 +15,32 @@ public final class Throttle: ThrottleProtocol {
     }
 
     /// Throttles a block that will be only executed if no later requests are sent within the specified interval of time (100 milliseconds by default)
-    public func throttle(block: EpicBlock?) {
+    public func throttle(block: @escaping EpicBlock) {
+        self.throttle(block: block, onQueue: .main)
+    }
+
+    private func throttle(
+        block: @escaping EpicBlock,
+        onQueue queue: DispatchQueue = .main
+    ) {
         self.task.cancel()
         self.task = DispatchWorkItem() { [weak self] in
             self?.previousExecution = Date()
-            DispatchQueue.main.async {
-                block?()
+            queue.async {
+                block()
             }
         }
-        let delay = Me.second(from: self.previousExecution) > self.interval ? 0 : self.interval
-        self.queue.asyncAfter(deadline: .now() + Double(delay), execute: self.task)
+        let delay = Throttle.second(from: self.previousExecution) > self.interval ? 0 : self.interval
+        self.queue.asyncAfter(
+            deadline: .now() + Double(delay),
+            execute: self.task
+        )
     }
 
-    private static func second(from referenceDate: Date) -> TimeInterval {
-        return Date().timeIntervalSince(referenceDate)
+    private static func second(
+        from referenceDate: Date,
+        now: Date = Date()
+    ) -> TimeInterval {
+        return now.timeIntervalSince(referenceDate)
     }
 }
